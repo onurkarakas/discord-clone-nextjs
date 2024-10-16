efd4e71cf16a177480fd537c11c760b24745a36e
Onur KARAKAŞ <onur9415@gmail.com>
2024-10-16 22:29:34 +0300

    redirect.

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 
+// Define the interface for the webhook request body
 interface WebhookRequestBody {
   data: {
     email_addresses: {
@@ -17,31 +18,21 @@ const allowedEmails: string[] = [
   "murathanklc@gmail.com",
   "lvntfdn@gmail.com",
   "gkhnkadioglu@gmail.com",
-  "",
-  "",
 ];
-
 export async function POST(req: Request) {
   try {
-    // Parse the request body as JSON
-    const body: WebhookRequestBody = await req.json();
+    const body: WebhookRequestBody = await req.json(); // Parse the JSON body with the defined interface
+    const userEmail = body.data.email_addresses[0].email_address;
 
-    // Extract the email address from the webhook payload
-    const userEmail: string = body.data.email_addresses[0].email_address;
-
-    // Check if the email is in the allowed list
     if (allowedEmails.includes(userEmail)) {
       // Allow the sign-in
       return NextResponse.json({ message: "User is allowed" }, { status: 200 });
     } else {
-      // Reject the sign-in
-      return NextResponse.json(
-        { message: "Unauthorized user" },
-        { status: 403 }
-      );
+      // Reject the sign-in and redirect
+      return NextResponse.redirect("https://bascord.com", 302);
     }
   } catch (error) {
-    // Handle any errors that occur
+    // Handle any errors
     return NextResponse.json(
       { message: "Server Error", error },
       { status: 500 }

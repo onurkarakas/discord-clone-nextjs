db7535cd6a911ac669649ed92e16b9046e4467e6
Onur KARAKAŞ <onur9415@gmail.com>
2024-10-16 21:53:00 +0300

    Added webook handler.

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+interface WebhookRequestBody {
+  data: {
+    email_addresses: {
+      email_address: string;
+    }[];
+  };
+}
+
+const allowedEmails: string[] = ["onur9415@gmail.com"];
+
+export async function POST(req: Request) {
+  try {
+    // Parse the request body as JSON
+    const body: WebhookRequestBody = await req.json();
+
+    // Extract the email address from the webhook payload
+    const userEmail: string = body.data.email_addresses[0].email_address;
+
+    // Check if the email is in the allowed list
+    if (allowedEmails.includes(userEmail)) {
+      // Allow the sign-in
+      return NextResponse.json({ message: "User is allowed" }, { status: 200 });
+    } else {
+      // Reject the sign-in
+      return NextResponse.json(
+        { message: "Unauthorized user" },
+        { status: 403 }
+      );
+    }
+  } catch (error) {
+    // Handle any errors that occur
+    return NextResponse.json(
+      { message: "Server Error", error },
+      { status: 500 }
+    );
+  }
+}
